878529300c866f6fa78de0395662f6434c5e08b6
Antonio Terreno <antonio.terreno@gmail.com>
2014-02-10 13:40:15 +0000

    factorial

--- MyModule.scala
+++ MyModule.scala
@@ -1,11 +1,28 @@
 object MyModule {
+
   def abs(n: Int): Int =
     if (n < 0) -n
     else n
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
-}
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  }
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    go(n, 1)
+  }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+  }
 }
